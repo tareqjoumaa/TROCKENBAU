3d00446e44c67a061a2968655e0d10a22faef243
tareq joumaa <tareqjoumaa4@gmail.com>
2024-08-29 00:06:25 +0300

    changes on service and contact

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "USEREMAIL@gmail.com";
+        $mail_to = "m.saleem.mourad@gmail.com";
         
         # Sender Data
         // $subject = trim($_POST["subject"]);
